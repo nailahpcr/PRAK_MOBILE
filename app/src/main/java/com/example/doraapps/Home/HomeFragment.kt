d46ae90a45ca3d4c4b065fc553429ada7d80c23c
rafi24si <rafi24si@mahasiswa.pcr.ac.id>
2026-05-20 10:07:22 +0700

    commitp10 kedua

--- app/src/main/java/com/example/doraapps/Home/HomeFragment.kt
+++ app/src/main/java/com/example/doraapps/Home/HomeFragment.kt
@@ -1,39 +1,86 @@
 package com.example.doraapps.Home.pertemuan10
+import android.content.Context.MODE_PRIVATE
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.doraapps.databinding.FragmentHomeBinding
 import com.example.doraapps.Home.pertemuan10.TenthActivity
+import com.example.doraapps.Home.pertemuan2.SecondActivity
+import com.example.doraapps.Home.pertemuan3.ThirdActivity
+import com.example.doraapps.Home.pertemuan4.FourthActivity
+import com.example.doraapps.Home.pertemuan9.NinthActivity
 
 class HomeFragment : Fragment() {
-
-    // Inisialisasi View Binding untuk Fragment
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
+        // Inflate the layout for this fragment
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
 
-        // Logika ketika tombol di klik
-        binding.btnPertemuan10.setOnClickListener {
-            val intent = Intent(requireContext(), TenthActivity::class.java)
-            startActivity(intent)
+        (requireActivity() as AppCompatActivity).setSupportActionBar(binding.toolbar)
+        (requireActivity() as AppCompatActivity).supportActionBar?.apply {
+            title = "Home"
         }
-    }
+        val sharedPref = requireContext().getSharedPreferences("session_user", MODE_PRIVATE)
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null // Menghindari memory leak
+        binding.btnPertemuan2.setOnClickListener {
+            startActivity(Intent(requireContext(), SecondActivity::class.java ))
+        }
+
+        binding.btnPertemuan3.setOnClickListener {
+            startActivity(Intent(requireContext(), ThirdActivity::class.java))
+        }
+
+        binding.btnPertemuan4.setOnClickListener {
+            startActivity(Intent(/* packageContext = */ requireContext(), /* cls = */ FourthActivity::class.java))
+        }
+
+        binding.btnPertemuan7.setOnClickListener {
+            startActivity(Intent(requireContext(), SeventhActivity::class.java))
+        }
+
+        binding.btnPertemuan9.setOnClickListener {
+            startActivity(Intent(requireContext(), NinthActivity::class.java))
+        }
+        binding.btnPertemuan10.setOnClickListener {
+            startActivity(Intent(requireContext(), TenthActivity::class.java))
+        }
+
+        // Fitur Logout
+        binding.btnLogout.setOnClickListener { //pakai binding untuk lebih mempermudah pemanggilan id
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle("Logout")
+                .setMessage("Apakah Anda yakin ingin Logout?")
+                .setPositiveButton("Ya") { dialog, _ ->
+                    sharedPref.edit {
+                        clear()
+                    }
+                    dialog.dismiss()
+
+                    val intent = Intent(requireContext(), AuthActivity::class.java)
+                    startActivity(intent)
+                    requireActivity().finish()
+
+                    dialog.dismiss()
+                    Log.e("Info Dialog","Anda memilih Ya!")
+                }
+                .setNegativeButton("Batal") { dialog, _ ->
+                    dialog.dismiss()
+                    Log.e("Info Dialog","Anda memilih Tidak!")
+                }
+                .show()
+        }
     }
 }
